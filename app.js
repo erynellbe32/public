6ee9f57d54e98b7204152c5d3c892836afefd720
Ray Evangelista <erynellbe@gmail.com>
2017-06-21 11:30:28 +0800

    update and codify

--- app.js
+++ app.js
@@ -91,7 +91,7 @@ server.get('/register', function(req, res) {
 
 
 
-    
+
 /*registerPOST CREATE NEW USER WITH EMAIL AND PASSWORD with ROUTE PARAMS and with Mongo DB, convert to Firebase */
 server.post('/api/user/:email/:password/', urlencodedParser, function (req, res, next) {
     var newUser = new User(); // todo experiment on user
@@ -119,11 +119,65 @@ server.post('/api/user/:email/:password/', urlencodedParser, function (req, res,
 
 
 
+  /*  TODO Firebase Admin */
+
+
+        admin.auth().createUser({
+            email: "user@example.com",
+            emailVerified: false,
+            password: "secretPassword",
+            displayName: "John Doe",
+            photoURL: "http://www.example.com/12345678/photo.png",
+            disabled: false
+        })
+        .then(function(userRecord) {
+            // See the UserRecord reference doc for the contents of userRecord.
+            console.log("Successfully created new user:", userRecord.uid);
+        })
+        .catch(function(error) {
+            console.log("Error creating new user:", error);
+        });
 
 
 
 
+    admin.auth().getUser(uid)
+        .then(function(userRecord) {
+            // See the UserRecord reference doc for the contents of userRecord.
+            console.log("Successfully fetched user data:", userRecord.toJSON());
+        })
+        .catch(function(error) {
+            console.log("Error fetching user data:", error);
+        });
 
+
+
+    admin.auth().getUserByEmail(email)
+        .then(function(userRecord) {
+            // See the UserRecord reference doc for the contents of userRecord.
+            console.log("Successfully fetched user data:", userRecord.toJSON());
+        })
+        .catch(function(error) {
+            console.log("Error fetching user data:", error);
+        });
+
+
+
+    admin.auth().updateUser(uid, {
+            email: "modifiedUser@example.com",
+            emailVerified: true,
+            password: "newPassword",
+            displayName: "Jane Doe",
+            photoURL: "http://www.example.com/12345678/photo.png",
+            disabled: true
+        })
+        .then(function(userRecord) {
+            // See the UserRecord reference doc for the contents of userRecord.
+            console.log("Successfully updated user", userRecord.toJSON());
+        })
+        .catch(function(error) {
+            console.log("Error updating user:", error);
+        });
 
 
         /*
